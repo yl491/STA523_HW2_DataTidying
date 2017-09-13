5290940588ef49f36921db72e8fa1c7c5141dede
Colin Rundel <rundel@gmail.com>
2017-09-13 12:37:51 -0400

    First draft

---
+++ hw2_whitelist.R
@@ -0,0 +1,25 @@
+library(stringr)
+library(purrr)
+
+allowed_files = c("hw2.Rmd",
+                  "hw2.Rproj",
+                  "hw2_whitelist.R",
+                  "README.md",
+                  "wercker.yml",
+                  "lego_sales.RData",
+                  "lego_sales.json",
+                  ".gitignore")
+
+files = dir()
+disallowed_files = !map_lgl(files, ~ any(str_detect(., allowed_files)))
+
+if (length(disallowed_files != 0))
+{
+  cat("Disallowed files found:\n")
+  cat("  (remove the following files from your repo)\n\n")
+
+  for(file in files[disallowed_files])
+    cat("*",file,"\n")
+
+  quit("no",1,FALSE)
+}
